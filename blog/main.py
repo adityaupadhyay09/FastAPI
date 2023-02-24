bff78ef4eb9e1caf865b2d69048b93977cb7d00c
adityaupadhyay-kiwi <aditya.upadhyay@kiwitech.com>
2023-02-24 15:05:04 +0530

    database connection

--- blog/main.py
+++ blog/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
+from .import schemas
 app = FastAPI()
 
 @app.post("/blog")
 
-class Blog(BaseModel):
-    title:str
-    blog:str
-    
-def create_blog(request:Blog):
-    return request
 
-# def create(title, blog):
-#     return {'title': title, 'blog': blog}
+    
+def create_blog(request:schemas.Blog):
+    return request
