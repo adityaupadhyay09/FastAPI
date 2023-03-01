e8d0d3495efcd9a93b53621bef44f4de9abc5691
adityaupadhyay-kiwi <aditya.upadhyay@kiwitech.com>
2023-03-01 11:53:20 +0530

    password and hashing

--- blog/main.py
+++ blog/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from .import schemas, models
 from . database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
+from .hashing import Hash
+
 
 app = FastAPI()
 
@@ -55,17 +58,28 @@ def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 # view the all blog entries
-@app.get("/blog")
+@app.get("/blog", response_model= List[schemas.ShowBlog])
 def all_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
 # view the all blog entries with the specified id
-@app.get("/blog/{id}", status_code=200)
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id, response:Response, db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blogs:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f'Blog with the id {id} not available')
     return blogs
+
+
+
+@app.post("/user")
+def create_user(request:schemas.Admin, db: Session = Depends(get_db)):
+    new_user = models.Admin(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,14 @@ from .database import Base
 
 class Blog(Base):
     __tablename__ = "blogs"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    
+    
+class Admin(Base):
+    __tablename__ = "admins"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
