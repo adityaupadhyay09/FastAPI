e287dea00e65d4438d96be9c185d5161fc00edff
adityaupadhyay-kiwi <aditya.upadhyay@kiwitech.com>
2023-03-01 18:51:55 +0530

    routers/bog/user

--- blog/main.py
+++ blog/main.py
@@ -1,96 +1,14 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
-from .import schemas, models
-from . database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from typing import List
-from .hashing import Hash
+from fastapi import FastAPI
+from .import models
+from . database import engine
+from .routers import blog, user
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+app.include_router(blog.router)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
+app.include_router(user.router)
 
-# created a blog post        
-@app.post("/blog", status_code=status.HTTP_201_CREATED, tags=['Blogs'])
-def create_blog(request:schemas.Blog, db: Session = Depends(get_db)):
-    new_blog  = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-
-# deleted a blog post
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=['Blogs'])
-def destroy(id, db: Session = Depends(get_db)):
-    blog=db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f'Blog with the id {id} not available')
-        
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return "Blog deleted successfully"
-    
-
-# update the blog with the specified id
-@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=['Blogs'])
-def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f'Blog with the id {id} not available')
-    blog.update({models.Blog.title: request.title, models.Blog.body: request.body})
-    db.commit()
-    return "Blog updated successfully"
-    
-
-
-
-# view the all blog entries
-@app.get("/blog", response_model= List[schemas.ShowBlog], tags=['Blogs'])
-def all_blog(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-
-# view the all blog entries with the specified id
-@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog, tags=['Blogs'])
-def show(id, response:Response, db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blogs:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f'Blog with the id {id} not available')
-    return blogs
-
-
-
-@app.post("/user", response_model=schemas.ShowUser, tags=['User'])
-def create_user(request:schemas.Admin, db: Session = Depends(get_db)):
-    new_user = models.Admin(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-
-@app.get("/user/{id}", response_model=schemas.ShowUser, tags=['User'])
-def get_user(id:int, db: Session = Depends(get_db)):
-    user = db.query(models.Admin).filter(models.Admin.id == id).first()
-    
-    if not user :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f'User with the id {id} not available')
-        
-    return user
---
+++ blog/routers/user.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter,  Depends, status, HTTPException, Response
+from ..import schemas, database, models
+from sqlalchemy.orm import Session
+from ..hashing import Hash
+
+router = APIRouter(
+    prefix="/user" ,
+    tags=['User']
+    )
+
+get_db = database.get_db
+
+
+@router.post("/", response_model=schemas.ShowUser)
+def create_user(request:schemas.Admin, db: Session = Depends(get_db)):
+    new_user = models.Admin(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+
+@router.get("/{id}", response_model=schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.Admin).filter(models.Admin.id == id).first()
+    
+    if not user :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'User with the id {id} not available')
+        
+    return user
+
